dcf04edd97727629461a5fe3912e27af25b03a72
Ian Taite <ian.taite@lexisnexisrisk.com>
2024-09-16 15:00:19 +0100

    Add skeleton list and save components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './movies/components/list/list.component';
+import { SaveComponent } from './movies/components/save/save.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent,
+  },
+  {
+    path:'create',
+    component: SaveComponent,
+  },
+  {
+    path: 'update/:id',
+    component: SaveComponent
+  }
+];
