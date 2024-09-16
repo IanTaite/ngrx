5453767e64220da825df36f3a687a470b5ac1705
Ian Taite <ian.taite@lexisnexisrisk.com>
2024-09-16 15:00:19 +0100

    Add skeleton list and save components and routes

---
+++ src/app/movies/components/save/save.component.ts
@@ -0,0 +1,13 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-save',
+  standalone: true,
+  imports: [],
+  templateUrl: './save.component.html',
+  styleUrl: './save.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SaveComponent {
+
+}
